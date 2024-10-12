529f2c7fa1df1f91f98f03c95849dd9f93bbf4ba
Guravareddy <guru2003jillella@gmail.com>
2024-10-12 09:07:47 +0530

    deployment

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,34 @@
-const express=require("express")
-const {app,server} =require('./socket/socket.js')
-const { default: mongoose } = require('mongoose')
-const cookiePraser=require("cookie-parser")
-const dotenv=require('dotenv')
-const mongooseConnection=require("./mongooseconnection.js")
-const authRouter=require("./routers/authRouter.js")
-const messagesRouter=require("./routers/messagesRouter.js")
-const userRouter=require("./routers/userRouter.js")
+// const express=require("express")
+const dotenv = require('dotenv');
+dotenv.config();
 
+const express = require("express");
+const { app, server } = require('./socket/socket.js');
+const mongoose = require('mongoose');
+const cookieParser = require("cookie-parser");
+const mongooseConnection = require("./mongooseconnection.js");
+const authRouter = require("./routers/authRouter.js");
+const messagesRouter = require("./routers/messagesRouter.js");
+const userRouter = require("./routers/userRouter.js");
+const path=require("path")
 
-
-dotenv.config()
-
+const port = process.env.PORT || 3000;
+console.log("Port:", process.env.PORT);
+console.log("MongoDB URL:", process.env.MONGO_DB_URL);
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
-app.use(cookiePraser())
+app.use(cookieParser())
 
 app.use('/api/',userRouter)
 app.use("/api/auth",authRouter)
 app.use("/api/auth/messages",messagesRouter)
 
-
-
-server.listen(3000,()=>{
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist","index.html"))
+})
+server.listen(port,()=>{
     mongooseConnection()
-    console.log("port is listening")
+    console.log("port is listening",port)
 })
--- backend/mongooseconnection.js
+++ backend/mongooseconnection.js
@@ -1,9 +1,14 @@
 const mongoose=require("mongoose")
+const dotenv=require("dotenv")
 
+dotenv.config()
 
 const mongooseConnection=async()=>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp')
+        console.log(process.env.PORT)
+        console.log(process.env.MONGO_DB_URL)
+        await mongoose.connect(process.env.MONGO_DB_URL)
+        console.log("connected to mongo")
     }catch(e){
         console.log("error in the mongoose connection")
     }
